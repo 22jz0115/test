adae688f53274063bc33e3bbc149e3c7766bb540
22jz0102 <22jz0102@jec.ac.jp>
2024-10-30 12:16:17 +0900

    できてるかわからないです

--- src/servlet/TaskInput.java
+++ src/servlet/TaskInput.java
@@ -38,6 +38,5 @@ public class TaskInput extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 		
-
 	}
 }
