c6a64a461cb37c37b3a0ae449d6bc1844752375b
22jz0102 <22jz0102@jec.ac.jp>
2024-11-13 14:07:57 +0900

    エラーページ作りました

--- src/servlet/TaskInput.java
+++ src/servlet/TaskInput.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,9 +10,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.CategoriesDAO;
+import dao.TasksDAO;
+import model.Accounts;
 import model.Categories;
+import model.Tasks;
 
 /**
  * Servlet implementation class taskinput
@@ -32,40 +38,42 @@ public class TaskInput extends HttpServlet {
    		
    		 request.getRequestDispatcher("/WEB-INF/jsp/taskInput.jsp").forward(request, response);
     }
-
     // POSTメソッド：タスクの追加処理
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
     	request.setCharacterEncoding("UTF-8");
         // フォームデータを取得
-        String date = request.getParameter("dateDisplay");
+        String date = request.getParameter("dateInput");
         String time = request.getParameter("appt-time");
-        String category = request.getParameter("task-1");
+        String category = request.getParameter("categorySelect"); //int型にする
         String taskName = request.getParameter("taskName");
         String memo = request.getParameter("story");
         
-        System.out.println(date);
-        System.out.println(time);
-        System.out.println(category);
-        System.out.println(taskName);
-        System.out.println(memo);
+        int categoryId = Integer.parseInt(category);
         
-    	//HttpSession session = request.getSession();
-        //Accounts loginUser = (Accounts) session.getAttribute("loginUser");
-       //int accountId = loginUser.getId();
+        String datetime = date + " " + time;
+        LocalDateTime taskDateTime = LocalDateTime.parse(datetime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        
+    	HttpSession session = request.getSession();
+        Accounts loginUser = (Accounts) session.getAttribute("loginUser");
+        int accountId = loginUser.getId();
+        
+        int outin = request.getParameter("switch") != null ? 1 : 0;
         
 
         // DAOを使ってデータベースにタスクを挿入
-       // TasksDAO dao = new TasksDAO();
-//        Tasks TaskInput = dao.create();
-//
-//        if (newTaskInput != null) {
-//            // 挿入が成功した場合、タスク一覧画面にリダイレクト
-//            response.sendRedirect("Task?date=" + date);  // 日付をクエリパラメータとして渡してリダイレクト
-//        } else {
-//            // 挿入が失敗した場合、エラーメッセージを表示
-//            response.getWriter().println("タスクの追加に失敗しました。");
-//        }
-        request.getRequestDispatcher("/WEB-INF/jsp/task.jsp").forward(request, response);
+        TasksDAO dao = new TasksDAO();
+        Tasks TaskInput = dao.create(categoryId, accountId, taskName, memo, outin, taskDateTime);
+
+        if (TaskInput != null) {
+            // 挿入が成功した場合、タスク一覧画面にリダイレクト
+            response.sendRedirect("Task?date=" + date);  // 日付をクエリパラメータとして渡してリダイレクト
+            System.out.println("okokokok");
+        } else {
+            // 挿入が失敗した場合、エラーメッセージを表示
+        	System.out.println("ngngngng");
+        	response.sendRedirect("Task?date=" + date);
+        }
+        
     }
 }
