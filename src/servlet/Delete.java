04f34e87bcccefd4ea54cb387c4cf93bbbe36803
22jz0102 <22jz0102@jec.ac.jp>
2024-11-22 11:45:45 +0900

    削除

--- src/servlet/Delete.java
+++ src/servlet/Delete.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.time.LocalDate;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,35 +17,33 @@ import model.Accounts;
  */
 @WebServlet("/Delete")
 public class Delete extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
 
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 HttpSession session = request.getSession();
-		    Accounts loginUser = (Accounts) session.getAttribute("loginUser");
-		    
-		     
-		    String taskIdStr = request.getParameter("taskId");
-		    int taskId = Integer.parseInt(taskIdStr);
-		    
-		 // パラメータから日付を取得
-	        String selectedDate = request.getParameter("selectedDate");
-	        
-	        if (selectedDate == null || selectedDate.isEmpty()) {
-	            LocalDate today = LocalDate.now();
-	            selectedDate = today.toString(); // YYYY-MM-DD形式
-	        }
-	        
-	        TasksDAO taskdao = new TasksDAO();
-	        taskdao.deleteTask(loginUser.getId(), taskId);
-	        
-	        request.getAttribute("selectedDate");
-	   		request.getRequestDispatcher("/WEB-INF/jsp/Tasks.jsp").forward(request, response);
-	   		
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         HttpSession session = request.getSession();
+            Accounts loginUser = (Accounts) session.getAttribute("loginUser");
+            
+             
+            String taskIdStr = request.getParameter("taskId");
+            int taskId = Integer.parseInt(taskIdStr);
+            
+         // パラメータから日付を取得
+            String selectedDate = request.getParameter("selectedDate");
+            
+           
+            System.out.println("Delete SelectedDate : " + selectedDate);
+            
+            TasksDAO taskdao = new TasksDAO();
+            taskdao.deleteTask(loginUser.getId(), taskId);
+            
+            request.getAttribute("selectedDate");
+            response.sendRedirect(request.getContextPath() + "/Task?date=" + selectedDate);
+               
+    }
 
 }
